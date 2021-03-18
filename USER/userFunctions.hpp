4376ef6aeee9c74c5e62de1c77529dd44428ecdb
Salbei <marc.heinze@muenster.de>
2021-03-18 17:33:57 +0100

    Added Gottesfinger Code

--- USER/userFunctions.hpp
+++ USER/userFunctions.hpp
@@ -2,3 +2,50 @@
 *   Hier können eigene Funktionen eingebunden werden.
 *   Ist in CfgFunctions included.
 */
+
+
+class grad_grandPrix
+{
+	tag = "grad_grandPrix";
+	class gottesFinger
+	{
+		file = "USER\functions\gottesFinger";
+		class gottesFinger_cancel {};
+		class gottesFinger_end {};
+		class gottesFinger_guess {};
+		class gottesFinger_hintGroup {};
+		class gottesFinger_initStation {};
+		class gottesFinger_openDisplay {};
+		class gottesFinger_prepNextPosition {};
+		class gottesFinger_start {};
+  	};
+	/*
+	class race
+	{
+		file = "USER\functions\race";
+		class addActions {};
+		class resetRace {};
+		class results {};
+		class startRace {};
+		class startTimeRecording {};
+		class stopTimeRecording {};
+  	};
+	class planke
+	{
+		file = "USER\functions\planke";
+		class addJumpEHs {};
+		class jumpResults {};
+		class plankGroupResult {};
+		class portToCarrier {};
+	};
+	*/
+	class points {
+		file = "USER\functions\points";
+		class addPoints {};
+		class addTime {};
+		class getGroup {};
+		class initGroups {};
+		class showResults {};
+		class showStages {};
+	};
+};
